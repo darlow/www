3119b79ffd061ab93cfddf06a9719bc68544cbfe
Ben Darlow <github@kapowaz.net>
2012-01-01 19:52:33 +0000

    Added www redirect

--- application.rb
+++ application.rb
@@ -3,10 +3,12 @@ require 'sinatra/base'
 require 'herbie'
 
 class Darlow < Sinatra::Base
-    
   set :views,                   File.dirname(__FILE__) + '/app/views'
   set :public,                  File.dirname(__FILE__) + '/public'
   
+  before do
+    redirect request.url.sub('http://', 'http://www.'), 301 unless request.host =~ /^www/
+  end
 end
 
 Dir[File.join(File.dirname(__FILE__), 'app/**/*.rb')].sort.each { |f| require f }
