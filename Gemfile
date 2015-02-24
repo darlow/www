ad28d0b2248f77f6ff95a58fd9b063b1e04b6370
Ben Darlow <github@kapowaz.net>
2015-02-24 15:35:23 +0000

    New design.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ ruby '2.1.5'
 
 gem 'rack'
 gem 'sinatra',                    github: 'sinatra/sinatra'
-gem 'herbie'
+gem 'herbie',                     '~> 0.3.3'
 gem 'sinatra-asset-pipeline',     require: 'sinatra/asset_pipeline'
 gem 'sass',                       '~> 3.3'
 gem 'compass',                    '1.0.0.alpha.19'
--- app/routes/home.rb
+++ app/routes/home.rb
@@ -1,10 +1,21 @@
 # encoding: utf-8
 class Darlow < Sinatra::Base
   before do
-    @page = { :title => "Darlow Technology" }
+    @page = { title: "Darlow Technology" }
   end
 
   get "/" do
+    @page[:keywords] = [
+      'html',
+      'javascript',
+      'css',
+      'scss',
+      'coffeescript',
+      'ruby',
+      'sinatra',
+      'sprockets',
+      'responsive web design'
+    ]
     erb :'pages/home'
   end
 end
